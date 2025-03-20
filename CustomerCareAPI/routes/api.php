629d50993d36c2d2fcfb80516cb0cd9d98ef30f2
eljassimi <abdelmouhaimineeljassimi@gmail.com>
2025-03-20 13:55:01 +0000

    create the api route for the tickets

--- CustomerCareAPI/routes/api.php
+++ CustomerCareAPI/routes/api.php
@@ -1,9 +1,14 @@
 <?php
 
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\TicketController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
 Route::post('/logout', [AuthController::class, 'logout'])->middleware('auth:sanctum');
+
+Route::middleware('auth:sanctum')->group(function () {
+    Route::apiResource('tickets', TicketController::class);
+});
