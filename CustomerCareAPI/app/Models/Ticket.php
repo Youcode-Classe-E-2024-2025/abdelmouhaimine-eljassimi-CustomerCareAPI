8050a478e99b323671b4cf62fabd6f0f02aea155
eljassimi <abdelmouhaimineeljassimi@gmail.com>
2025-03-20 13:26:03 +0000

    create the ticket controller

--- CustomerCareAPI/app/Models/Ticket.php
+++ CustomerCareAPI/app/Models/Ticket.php
@@ -6,5 +6,6 @@ use Illuminate\Database\Eloquent\Model;
 
 class Ticket extends Model
 {
-    //
+    protected $table = 'tickets';
+    protected $fillable = ["user_id","agent_id","title","description","status","priority"];
 }
