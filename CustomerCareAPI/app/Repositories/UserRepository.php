a0c97b0a9147711977a6132d947b4cefd6829a3f
eljassimi <abdelmouhaimineeljassimi@gmail.com>
2025-03-20 11:42:34 +0000

    create function that create new user

--- CustomerCareAPI/app/Repositories/UserRepository.php
+++ CustomerCareAPI/app/Repositories/UserRepository.php
@@ -6,4 +6,6 @@ use App\Models\User;
 
 class UserRepository{
     public function findByEmail($email){ return User::where('email', $email)->first();}
+
+    public function create(array $data){return User::create($data);}
 }
