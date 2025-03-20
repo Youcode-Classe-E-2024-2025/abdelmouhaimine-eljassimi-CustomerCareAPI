d5428c250d0a890faae9f9d861298a9ec50e7fcb
eljassimi <abdelmouhaimineeljassimi@gmail.com>
2025-03-20 13:47:49 +0000

    create the index function that return all the tickets in the db

--- CustomerCareAPI/app/Http/Controllers/TicketController.php
+++ CustomerCareAPI/app/Http/Controllers/TicketController.php
@@ -2,9 +2,20 @@
 
 namespace App\Http\Controllers;
 
+use App\Services\TicketService;
 use Illuminate\Http\Request;
 
 class TicketController extends Controller
 {
+    protected $ticketService;
 
+    public function __construct(TicketService $ticketService)
+    {
+        $this->ticketService = $ticketService;
+    }
+
+    public function index()
+    {
+        return response()->json($this->ticketService->getAllTickets());
+    }
 }
