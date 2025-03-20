aca8a669ca435bed83bd43e2d09644eec129db51
eljassimi <abdelmouhaimineeljassimi@gmail.com>
2025-03-20 12:42:00 +0000

    login works correctly

--- CustomerCareAPI/app/Http/Requests/LoginUserRequest.php
+++ CustomerCareAPI/app/Http/Requests/LoginUserRequest.php
@@ -22,7 +22,7 @@ class LoginUserRequest extends FormRequest
     public function rules(): array
     {
         return [
-            'email' => 'required|string|email|unique:users',
+            'email' => 'required|string|email',
             'password' => 'required|string|min:6',
         ];
     }
